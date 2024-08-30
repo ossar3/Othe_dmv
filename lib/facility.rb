c1b7476b5a219f2f8eeb6b3d6bce1fa83aa0aa6c
Oscar Patterson <oepatterson12@gmail.com>
2024-08-30 16:08:43 -0400

    facility updat

--- lib/facility.rb
+++ lib/facility.rb
@@ -7,9 +7,28 @@ class Facility
     @address = facility[:address]
     @phone = facility[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
   end
+
+  
+
+  def register_vehicle(vehicle)
+#check if it has the service? IF
+  @registered_vehicles << vehicle
+  if vehicle.antique? == true
+    vehicle.plate_type = :antique
+    #if vehicle antique collect 25 else collect 100?
+    #set date
+  end
+  end
+
+
+  def
+
+
 end

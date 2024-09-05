4a717191682a252ae6fbf35401e7fab563fd6bae
Oscar Patterson <oepatterson12@gmail.com>
2024-09-05 08:09:50 -0400

    it2done

--- lib/factory.rb
+++ lib/factory.rb
@@ -3,5 +3,16 @@ class Factory
     def initialize
     end
 
-    def create_vehicles(data)
-        
+    def create_vehicles(cars)
+
+        cars.map do |car|
+            Vehicle.new(
+            vin: car[:vin_1_10],
+            year: car[:model_year].to_i,
+            make: car[:make],
+            model: car[:model],
+            engine: :ev
+             )
+        end
+    end
+end
--- spec/factory_spec.rb
+++ spec/factory_spec.rb
@@ -2,8 +2,24 @@ require 'spec_helper'
 
 RSpec.describe Factory do 
 
+    describe '#initialize' do
+        it 'can make factory' do
 
+            factory = Factory.new
 
+            expect(factory).to be_an_instance_of(Factory)
+        end
+    end
 
+    describe 'create' do
+        it 'can create vehicles' do
+            factory = Factory.new
+            wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+
+          vehicles = factory.create_vehicles(wa_ev_registrations)
+          expect(vehicles).to be_an(Array)
+            expect(vehicles).to all(be_a(Vehicle))
+        end
+    end
 
 end
